8270d087c0c6b044edcda92459b3cac3958b6f9e
Amber lu <1114amber@gmail.com>
2022-04-06 22:14:12 +0800

    'calculate

--- Read.py
+++ Read.py
@@ -4,9 +4,12 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0: # % 用來求餘數
+		if count % 1000 == 0: 
 			print(len(data))
-#利用計數來知道讀取狀態
-print(len(data))
+print('檔案讀取完了', '總共有', len(data), '筆資料')
 
-print(data[0])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	
+print('留言的平均長度是', sum_len/len(data), '個字')
