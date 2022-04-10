442ca38e202c15f80ff411f99988caf2e8f1e60e
Amber lu <1114amber@gmail.com>
2022-04-10 23:32:05 +0800

    another filtering

--- Read1.py
+++ Read1.py
@@ -21,4 +21,11 @@ for d in data:
 		new.append(d)
 print('一共有', len(new), '筆留言長度小於100字')
 print(new[0])
-		
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆資料含有good')
+print(good[0])
