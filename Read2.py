cb7bb04f5c5e55d4db7cdaaf6aee11852960eb59
Amber lu <1114amber@gmail.com>
2022-05-18 20:41:54 +0800

    add word count

--- Read2.py
+++ Read2.py
@@ -1,6 +1,6 @@
 data = []
 count = 0
-with open('reviews.txt', 'r') as f:
+with open('reviews.txt', 'r') as f:  #'r'表示讀取模式，'w'表示寫入模式
 	for line in f:
 		data.append(line)
 		count += 1
@@ -8,10 +8,10 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了', '總共有', len(data), '筆資料')
 
+
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-	
 print('留言的平均長度是', sum_len/len(data), '個字')
 
 
@@ -23,8 +23,41 @@ print('一共有', len(new), '筆留言長度小於100字')
 print(new[0])
 
 
-good = [d for d in data if'good' in d]
+good = [d for d in data if 'good' in d]
 #第一個d代表若符合條件就把什麼東西裝進去，後面則表條件
 print('一共有', len(good), '筆留言提到good')
 
-bad = ['bad' in d for d in data] #清單裡會裝True 或 false
+
+#文字計數
+wc = {}  #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1    #新增key進去wc
+
+for word in wc:
+	if wc[word] > 1000000:
+	    print(word, wc[word])
+
+print(len(wc))
+print(wc['Amber'])
+
+while True:
+	word = input('請問你想要查甚麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為', wc[word])
+	else:
+		print('這個字沒有出現過喔!')
+
+
+print('感謝使用本查詢功能')
+
+
+bad = ['bad' in d for d in data] #清單裡會裝True 或 false 分析每一筆留言裡是否有bad
+
+
